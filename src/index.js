03b60341a4fb1565bdf522f33385b05fb756d082
Veronika <verona.binko@gmail.com>
2021-03-11 23:39:15 +0300

    feat: complete task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!arguments.length || !array.length) return 0;
+
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!arguments.length || !array.length) return 0;
+
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!arguments.length || !array.length) return 0;
+
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+  result = result + array[i];
+  }
+
+  return (result / array.length);
 }
